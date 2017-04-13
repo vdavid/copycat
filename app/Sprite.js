b07f1dbb6ae3f278b1eea344d547bec893e1e3f6
Veszelovszki Dávid <veszelovszki@gmail.com>
2017-04-13 20:50:10 +0200

    Refactored Sprite too.

--- app/Sprite.js
+++ app/Sprite.js
@@ -2,40 +2,29 @@ export class Sprite {
     /**
      *
      * @param {CanvasRenderingContext2D} context
-     * @param {Number} tileSize
-     * @param {Number} positionX
-     * @param {Number} positionY
+     * @param {Number} positionXInPixels
+     * @param {Number} positionYInPixels
      * @param {Symbol} spriteId
      * @param {SpriteService} spriteService
      */
-    constructor(context, tileSize, positionX, positionY, spriteId, spriteService) {
-        this.context = context;
-        this.spriteId = spriteId;
-        this.spriteService = spriteService;
-        this.positionXInPixels = positionX * tileSize;
-        this.positionYInPixels = positionY * tileSize;
-        this.length = this.spriteService.getSpriteSheet(spriteId).columnCount;
-        this.frame = 0;
-        this.selectLigne = 0;
-        this.animation = true;
-        this.allure = 0.2;
+    constructor(context, positionXInPixels, positionYInPixels, spriteId, spriteService) {
+        this._context = context;
+        this._spriteId = spriteId;
+        this._spriteService = spriteService;
+        this.positionXInPixels = positionXInPixels;
+        this.positionYInPixels = positionYInPixels;
+        this._length = this._spriteService.getSpriteSheet(spriteId).columnCount;
+        this._frame = 0;
     }
-
-    draw() {
-        this.spriteService.draw(this.spriteId, this.context, this.positionXInPixels, this.positionYInPixels, Math.floor(this.frame), 0);
-    }
-
-    animate() {
-        if (this.animation) {
-            this.frame += this.allure;
-            if (this.frame >= this.length) {
-                this.frame = 0;
-            }
-        }
-    }
-
+    
     render() {
-        this.animate();
-        this.draw();
+        this._frame += 0.2;
+        if (this._frame >= this._length) {
+            this._frame = 0;
+        }
+
+        this._spriteService.draw(this._spriteId, this._context,
+            this.positionXInPixels, this.positionYInPixels,
+            Math.floor(this._frame), 0);
     }
 }
